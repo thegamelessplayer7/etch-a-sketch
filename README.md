ee8aca14def3d8034473884ebcbf518cdcb22f0e
The Gameless Player <thegamelessplayer7@gmail.com>
2021-10-01 13:29:42 -0500

    Add all files

---
+++ README.md
@@ -0,0 +1,6 @@
+Etch-a-Sketch by thegamelessplayer
+
+Etch-a-Sketch is a project of the fundamentals portion of the JavaScript lessons. This exercise uses DOM manipulation. 
+
+Feel free to email me!
+thegamelessplayer7@gmail.com
---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+create a container div inside of the index.html file
+
+add hover class on hover
+
+create grid of divs and append them to the container div
+
+use flexbox to make divs into grid
+
+
+
+
+*/
+newColor = 'red';
+let square;
+for (let i = 0; i < 30; i++) {
+    square = document.createElement('div');
+    square.className = 'new';
+    container.appendChild(square);
+}
+
+squareDivs = document.querySelectorAll('.new');
+
+for (let i = 0; i < squareDivs.length; i++) {
+    squareDivs[i].addEventListener('mouseover', function() {
+        squareDivs[i].classList.add('hover')
+    })
+}
