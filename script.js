7a0a2212fb3ff94420f1d2e86dd4ef8c99b3c832
The Gameless Player <thegamelessplayer7@gmail.com>
2021-10-01 14:19:05 -0500

    Adding JS CSS

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ use flexbox to make divs into grid
 
 
 */
-newColor = 'red';
+const grid = document.createElement('div');
+container.appendChild(grid);
+grid.classList.add('grid');
+
+
 let square;
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 16; i++) {
     square = document.createElement('div');
     square.className = 'new';
-    container.appendChild(square);
+    grid.appendChild(square);
 }
+for (let i = 0; i < 16; i++) {
+    squareTwo = document.createElement('div');
+    squareTwo.className = 'new';
+    grid.appendChild(squareTwo);
+}
+
 
 squareDivs = document.querySelectorAll('.new');
 
