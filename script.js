e351c1a587895dda53275ef04b6ced09eec588f0
The Gameless Player <thegamelessplayer7@gmail.com>
2021-10-07 13:37:00 -0500

    Random color working for the first time

--- script.js
+++ script.js
@@ -27,24 +27,64 @@ function makeGrid(rows, cols) {
 makeGrid(16, 16);
 
 grid.addEventListener('mouseover', function(e) {
-    if(e.target.className === 'reg') {
+    if(e.target.className === 'reg')  {
         e.target.classList.add('hover');
     }
-})
+})  
 
 
 const clearBtn = document.getElementById('clr-button')
-
+const squareDivs = document.querySelectorAll('.reg');
 clearBtn.addEventListener('click', () => {
-    squareDivs = document.querySelectorAll('.reg');
+    
     for (let i = 0; i < squareDivs.length; i++) {
     squareDivs[i].classList.remove('hover');
     }
 })  
 
+function getRandomColor() {
+    let randomColor = '#';
+    randomColor += Math.floor(Math.random()*16777215).toString(16);
+    for (let i = 0; i < squareDivs.length; i++) {
+    console.log(squareDivs[i]);
+        //squareDivs[i].style.backgroundColor = randomColor;
+    }
+}
+
+function pickColor() {
+    squareDivs.style.backgroundColor = getRandomColor();
+    console.log(squareDivs.style.backgroundColor);
+}
+
+
+
+const randomize = document.getElementById('randomize');
+randomize.addEventListener('click', () => {
+    for(let i = 0; i < squareDivs.length; i++) {
+        squareDivs[i].onmouseover = function(e) {
+            let colorRandom = '#'+Math.floor(Math.random()*16777215).toString(16);
+            let colorString = '0px 0px 30px 0px' + colorRandom;
+            this.style['background-color'] = colorRandom;
+        }
+    } 
+}
+);
+
+
 const squaresBtn = document.getElementById('squares-btn');
 
 squaresBtn.addEventListener('click', () => {
     let num = prompt('How many squares would you like to add?');
-    makeGrid(num, num);
+    if (num > 100) {
+        num = prompt('Don\'t enter a number greater than 100!');
+    } else if (num < 1) {
+        num = prompt('Enter a number greater than 1!');
+    } else if (num <= 100) {
+        makeGrid(num, num);
+    }
 })
+
+
+document.querySelector('.random').style.backgroundColor = getRandomColor(); 
+
+
